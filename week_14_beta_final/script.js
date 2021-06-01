2a543bfca06fe7bcb9a0bd519dfa1d120b705ef4
Anastasiia <65309242+stasysiia@users.noreply.github.com>
2021-05-31 22:54:14 -0700

    commit on main

---
+++ week_14_beta_final/script.js
@@ -0,0 +1,13 @@
+var myModal = new bootstrap.Modal(document.getElementById("myModal"), {});
+
+function show(){
+  myModal.show();
+}
+function hide(){
+  myModal.hide();
+}
+
+/*document.onreadystatechange = function () {
+  myModal.show();
+};*/
+
